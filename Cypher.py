f30a19be78155d481dffd81fb114d1a745ee8e33
Yijun Yan <yyan9023@shared.sydney.edu.au>
2020-01-07 21:40:34 +1100

    Weakened cypher

--- Cypher.py
+++ Cypher.py
@@ -5,22 +5,22 @@ class Cypher:
     def __init__(self):
         pass
 
-    def _pre_process_key(self, key, master):
-        key = list(key)
-        for x in range(len(key)): # Pre-process the key. Get each character of the key's Unicode value (1), and each character of the masterkey's Unicode value (2).
-            # Loop through, if the index%2 == 0, it will perform (1) - (2), otherwise plus
-            if x % 2:
-                key[x] = chr(
-                    abs(ord(key[x]) + ord(master[x % len(master)])) % 65536) 
-            else:
-                key[x] = chr(
-                    abs(ord(key[x]) - ord(master[x % len(master)])) % 65536)
-            # considering that it might exceed the range of unicode, do a %.
-            # considering that it might <0, get abs()
-        k = ''
-        for x in key:
-            k += x
-        return k
+    # def _pre_process_key(self, key, master):
+    #     key = list(key)
+    #     for x in range(len(key)): # Pre-process the key. Get each character of the key's Unicode value (1), and each character of the masterkey's Unicode value (2).
+    #         # Loop through, if the index%2 == 0, it will perform (1) - (2), otherwise plus
+    #         if x % 2:
+    #             key[x] = chr(
+    #                 abs(ord(key[x]) + ord(master[x % len(master)])) % 65536) 
+    #         else:
+    #             key[x] = chr(
+    #                 abs(ord(key[x]) - ord(master[x % len(master)])) % 65536)
+    #         # considering that it might exceed the range of unicode, do a %.
+    #         # considering that it might <0, get abs()
+    #     k = ''
+    #     for x in key:
+    #         k += x
+    #     return k
 
     def _encrypt(self, string, key):
         string = list(string)
@@ -58,13 +58,14 @@ class Cypher:
 
         return k
 
-    def encrypt(self, string, key, master):
-        k = self._encrypt(string, self._pre_process_key(key, master))
+    def encrypt(self, string, key):
+        # k = self._encrypt(string, self._pre_process_key(key, master))
+        k = self._encrypt(string, key)
         return k
 
-    def decrypt(self, string, key, master):
+    def decrypt(self, string, key):
         string = string.split('::')
-        key = self._pre_process_key(key,master)
+        # key = self._pre_process_key(key,master)
         return self._decrypt(string[0], key, string[1])
 
 
@@ -73,15 +74,15 @@ class Cypher:
 def encrypt(string, key = 'yyj:li'):
 
     a = Cypher()
-    key = key.split(':')
-    k, master = key
-    return a.encrypt(string, k, master)
+    # key = key.split(':')
+    # k, master = key
+    return a.encrypt(string, key)
 
 def decrypt(string, key = 'yyj:li'):
     a = Cypher()
-    key = key.split(':')
-    k, master = key
-    return a.decrypt(string, k, master)
+    # key = key.split(':')
+    # k, master = key
+    return a.decrypt(string, key)
 
 # print(encrypt('1234','123:34'))
 # print(decrypt('511525154::2322','123:34'))
